22a9627a7c248e9485a4e18dc92147264c7ab177
tiserle <tiserle@gmail.com>
2021-03-10 17:09:28 +0800

    Add files via upload
    Due to index change of reboot (or switch-over rpsf)

--- include/discovery/storage/seos.inc.php
+++ include/discovery/storage/seos.inc.php
@@ -1,22 +1,23 @@
 <?php
-/*
- * Copyright (c) 2017 Dave Bell <me@geordish.org>
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.  Please see LICENSE.txt at the top level of
- * the source code distribution for details.
- */
+
 if ($device['os'] == 'seos') {
-    $fstype = snmp_get($device, 'rbnSRStorageMedia.41', '-OvQ', 'RBN-SYS-RESOURCES-MIB');
-    $descr = snmp_get($device, 'rbnSRStorageDescr.41', '-OvQ', 'RBN-SYS-RESOURCES-MIB');
+    $dsktable_array = snmpwalk_cache_oid($device, 'rbnSRStorageDescr', null, 'RBN-SYS-RESOURCES-MIB'); 
+    //print_r($dsktable_array);
+    foreach($dsktable_array as $key => $line)
+    {
+     if(preg_match("/Var Disk/",$line[rbnSRStorageDescr]))
+     {
+      $disk = $key;
+     }
+    }
+    $fstype = snmp_get($device, "rbnSRStorageMedia.$disk", '-OvQ', 'RBN-SYS-RESOURCES-MIB');
+    $descr = snmp_get($device, "rbnSRStorageDescr.$disk", '-OvQ', 'RBN-SYS-RESOURCES-MIB');
     $units  = 1024;
-    $index = 0;
-    $percent = snmp_get($device, 'rbnSRStorageUtilization.41', '-OvQ', 'RBN-SYS-RESOURCES-MIB');
-    $total = snmp_get($device, 'rbnSRStorageSize.41', '-OQUsnv', 'RBN-SYS-RESOURCES-MIB') * $units;
+    $percent = snmp_get($device, "rbnSRStorageUtilization.$disk", '-OvQ', 'RBN-SYS-RESOURCES-MIB');
+    $total = snmp_get($device, "rbnSRStorageSize.$disk", '-OQUsnv', 'RBN-SYS-RESOURCES-MIB') * $units;
     $used = $total * $percent / 100;  
     $free = $total - $used;
-    //echo "$total, $used";
+    $index = 0;
     if (is_numeric($free) && is_numeric($total)) {
         discover_storage($valid_storage, $device, $index, $fstype, 'seos', $descr, $total, $units, $used);
     }
