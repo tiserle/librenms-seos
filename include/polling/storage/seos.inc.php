ee390d20f27b310c451c28ab61fc20362149cdc9
tiserle <tiserle@gmail.com>
2021-03-10 17:13:38 +0800

    Update seos.inc.php

--- include/polling/storage/seos.inc.php
+++ include/polling/storage/seos.inc.php
@@ -1,17 +1,17 @@
 <?php
-/*
- * Copyright (c) 2017 Dave Bell <me@geordish.org>
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.  Please see LICENSE.txt at the top level of
- * the source code distribution for details.
- */
 
 if ($device['os'] == 'seos') {
+  $dsktable_array = snmpwalk_cache_oid($device, 'rbnSRStorageDescr', null, 'RBN-SYS-RESOURCES-MIB');
+    foreach($dsktable_array as $key => $line)
+    {
+     if(preg_match("/Var Disk/",$line[rbnSRStorageDescr]))
+     {
+      $disk = $key;
+     }
+    }
     $storage['units'] = 1024;
-    $storage['size'] = snmp_get($device, 'rbnSRStorageSize.41', '-OQUsnv', 'RBN-SYS-RESOURCES-MIB') * $storage['units'];
-    $percent = snmp_get($device, 'rbnSRStorageUtilization.41', '-OvQ', 'RBN-SYS-RESOURCES-MIB');
+    $storage['size'] = snmp_get($device, "rbnSRStorageSize.$disk", '-OQUsnv', 'RBN-SYS-RESOURCES-MIB') * $storage['units'];
+    $percent = snmp_get($device, "rbnSRStorageUtilization.$disk", '-OvQ', 'RBN-SYS-RESOURCES-MIB');
     $storage['used'] = $storage['size'] * $percent / 100;
     $storage['free'] = $storage['size'] - $storage['used'];
 }
