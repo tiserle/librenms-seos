eca1e28ef3aab52b1ff1977ae571c0a8732b9f02
tiserle <tiserle@gmail.com>
2021-03-11 13:32:47 +0800

    Update seos.inc.php

--- include/polling/mempools/seos.inc.php
+++ include/polling/mempools/seos.inc.php
@@ -4,9 +4,10 @@
 
 echo 'SmartEdge(SSR) MemPool'.'\n';
 $used = snmp_get($device, '.1.3.6.1.4.1.2352.2.16.1.2.1.4.1', '-OvQ');
-$user = $used * 1000;
 $free = snmp_get($device, '.1.3.6.1.4.1.2352.2.16.1.2.1.3.1', '-OvQ');
-$free = $free * 1000;
+$units = 1024;
+$used = $used * $units;
+$free = $free * $units;
 $mempool['total'] = ($free + $used);
 $mempool['free']  = $free;
 $mempool['used']  = $used;
