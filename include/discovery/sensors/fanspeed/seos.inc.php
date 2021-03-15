88063daabc7ea9de268ebb84ec168a3d166de044
tiserle <tiserle@gmail.com>
2021-03-15 18:58:08 +0800

    Update seos.inc.php

--- include/discovery/sensors/fanspeed/seos.inc.php
+++ include/discovery/sensors/fanspeed/seos.inc.php
@@ -1,61 +1,42 @@
 <?php
-/*
- * LibreNMS QNAP NAS Fanspeeds information module
- *
- * Copyright (c) 2016 Cercel Valentin <crc@nuamchefazi.ro>
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.  Please see LICENSE.txt at the top level of
- * the source code distribution for details.
- */
 
 echo 'SmartEdge(SSR): ';
 
+// Fan Tray 1
 $fan_descr_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.2.1';
 $fan_speed_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3.1';
+$temp_descr = snmpwalk_cache_numerical_oid($device, $fan_descr_oid, [], null, null, '-OQUsn');
+$temp_speed = snmpwalk_cache_numerical_oid($device, $fan_speed_oid, [], null, null, '-OQUsn');
 
-$fans_descr = snmpwalk_cache_numerical_oid($device, $fan_descr_oid, [], null, null, '-OQUsn');
-$fans_speed = snmpwalk_cache_numerical_oid($device, $fan_speed_oid, [], null, null, '-OQUsn');
-
-if (is_array($fans_speed) && !empty($fans_speed)) {
-    foreach ($fans_speed as $index => $entry) {
-        $tmp = $fan_speed_oid . '.' . $index;
-        $oid[] = $fan_speed_oid . '.' . $index;
-        $fan_oid = $fan_descr_oid . '.' . $index;
-        $fan_speed[] = $entry[$tmp];
-        $fan_serial[] = $fans_descr[$index][$fan_oid];
-	$temp[] = $index;
-    }
-}
-
+// Fan Tray 2
 $fan_descr_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.2.2';
 $fan_speed_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3.2';
-
 $fans_descr = snmpwalk_cache_numerical_oid($device, $fan_descr_oid, [], null, null, '-OQUsn');
 $fans_speed = snmpwalk_cache_numerical_oid($device, $fan_speed_oid, [], null, null, '-OQUsn');
 
+$fans_descr = array_merge($temp_descr, $fans_descr);
+$fans_speed = array_merge($temp_speed, $fans_speed);
+
+$fan_descr_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.2';
+$fan_speed_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3';
+
+$fan_descr_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.2';
+$fan_speed_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3';
+
 if (is_array($fans_speed) && !empty($fans_speed)) {
     foreach ($fans_speed as $index => $entry) {
-        $tmp = $fan_speed_oid . '.' . $index;
-        $oid[] = $fan_speed_oid . '.' . $index;
-        $fan_oid = $fan_descr_oid . '.' . $index;
-        $fan_speed[] = $entry[$tmp];
-        $fan_serial[] = $fans_descr[$index][$fan_oid];
-	$temp[] = $index+7;
-    }
-}
-/*
-print_r($oid);
-print_r($fan_speed);
-print_r($fan_serial);
-print_r($temp);
-*/
-$count = count($oid);
-for($i=0;$i<$count;$i++)
-{
- discover_sensor($valid['sensor'], 'fanspeed', $device, $oid[$i], $temp[$i], 'snmp', $fan_serial[$i], '1', '1', null, null, null, null, $fan_speed[$i]);
+        $leading = floor($index/7)+1;
+        $remainder = $index%7;
+        $append = $leading.".".$remainder;
+        $oid = $fan_speed_oid . '.' . $append;
+        $fan_oid = $fan_descr_oid . '.' . $append;
+        $fan_speed = $entry[$oid];
+        $fan_serial = $fans_descr[$index][$fan_oid];
+        if ($fan_speed) {
+            discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, 'snmp', $fan_serial, '1', '1', null, null, null, null, $fan_speed);
+        }
 
+    }
 }
 
 unset(
