0587e2d273023949a279a83874689eb607ea3abb
tiserle <tiserle@gmail.com>
2021-03-16 11:19:32 +0800

    Update seos.inc.php

--- include/discovery/sensors/fanspeed/seos.inc.php
+++ include/discovery/sensors/fanspeed/seos.inc.php
@@ -1,37 +1,27 @@
 <?php
 
+/*
+ * LibreNMS Ericsson SSR Fanspeeds information module
+ *
+ * Copyright (c) 2021 Kuang-Li Ting <tiserle@gmail.com>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
+
 echo 'SmartEdge(SSR): ';
-
-// Fan Tray 1
-$fan_descr_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.2.1';
-$fan_speed_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3.1';
-$temp_descr = snmpwalk_cache_numerical_oid($device, $fan_descr_oid, [], null, null, '-OQUsn');
-$temp_speed = snmpwalk_cache_numerical_oid($device, $fan_speed_oid, [], null, null, '-OQUsn');
-
-// Fan Tray 2
-$fan_descr_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.2.2';
-$fan_speed_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3.2';
-$fans_descr = snmpwalk_cache_numerical_oid($device, $fan_descr_oid, [], null, null, '-OQUsn');
-$fans_speed = snmpwalk_cache_numerical_oid($device, $fan_speed_oid, [], null, null, '-OQUsn');
-
-$fans_descr = array_merge($temp_descr, $fans_descr);
-$fans_speed = array_merge($temp_speed, $fans_speed);
-
-$fan_descr_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.2';
-$fan_speed_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3';
-
-$fan_descr_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.2';
-$fan_speed_oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3';
+$fans_descr = snmpwalk_cache_multi_oid($device, 'rbnFanUnitDescr', [], 'RBN-ENVMON-MIB');
+$fans_speed = snmpwalk_cache_multi_oid($device, 'rbnFanSpeedCurrent', [], 'RBN-ENVMON-MIB');
 
 if (is_array($fans_speed) && !empty($fans_speed)) {
     foreach ($fans_speed as $index => $entry) {
-        $leading = floor($index/7)+1;
-        $remainder = $index%7;
-        $append = $leading.".".$remainder;
-        $oid = $fan_speed_oid . '.' . $append;
-        $fan_oid = $fan_descr_oid . '.' . $append;
-        $fan_speed = $entry[$oid];
-        $fan_serial = $fans_descr[$index][$fan_oid];
+        $fan_serial = $fans_descr[$index]["rbnFanUnitDescr"];
+        $fan_speed = $fans_speed[$index]["rbnFanSpeedCurrent"];
+        $oid = '.1.3.6.1.4.1.2352.2.4.1.5.1.3';
+        $oid = $oid .".$index";
         if ($fan_speed) {
             discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, 'snmp', $fan_serial, '1', '1', null, null, null, null, $fan_speed);
         }
